e8e59e8d20c37daf283ba02e84ec11b1b5f05e89
MirageMK <sandro_popovski@yahoo.com>
2014-01-31 15:59:02 +0100

    Style changes

--- ProjectFZOM/App_Start/BundleConfig.cs
+++ ProjectFZOM/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace ProjectFZOM
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
+            bundles.Add(new StyleBundle("~/Content/mobilecss").Include("~/Content/jquery.mobile*"));
+
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
                         "~/Content/themes/base/jquery.ui.resizable.css",
